ba7f2ddfc3f49393db0a8902c96c1e818e15830e
amankul8 <amankul.981108@gmail.com>
2025-07-04 17:57:28 +0600

    Change card

--- frontend/app/shared/components/card/index.tsx
+++ frontend/app/shared/components/card/index.tsx
@@ -13,26 +13,35 @@ type CardType = {
 export default function Card({title, text, logo, rating, isLock, reviews, price}: CardType) {
     return (
         <div className="
-            border rounded-[5px] border-[var(--light-blue)] 
+            border rounded-[10px] border-[var(--light-blue)] 
             bg-[var(--ghost-white)] 
-            w-[365px] p-[16px]
+            w-[365px] px-[15px] py-[20px]
             flex flex-col gap-[16px]    
             md:w-[450px]
-            lg:w-[550px]
+            lg:w-[550px] lg:p-[20px]
             xl:w-[650px]
             2xl:w-[760px]
         ">
 
-            <div className="flex gap-[14px] justify-between">
-                <div className="w-[60px] h-[60px] bg-white rounded-[5px] flex items-center justify-center">
-                    <img className="h-[38] w-[26]" src={logo} alt="" />
+            <div className="flex gap-[15px] lg:gap[20px] justify-between">
+                <div className="w-[60px] h-[60px] bg-white rounded-[5px] flex items-center justify-center flex-none
+                    lg:w-[90px] lg:h-[90px]
+                ">
+                    <img className="h-[33px] lg:h-[60px]" src={logo} alt="" />
                 </div>
-                <div className="flex flex-col gap-[8px] w-full">
+                <div className="flex flex-col gap-[12px] lg:gap-[15px] w-full">
                     <div>
-                        <h2 className="inline font-[manrope] text-[var(--dark)] text-[20px] font-semibold">
+                        <h2 className="inline font-[manrope] text-[var(--dark)] text-[20px]/1.5 font-semibold">
                             {title}
                         </h2>
-                        <img className="w-[16px] h-[16px] inline ml-[8px]" src="/images/icons/arrow.png" alt="" />
+                        <img className="w-[14px] h-[14px] inline ml-[8px]" src="/images/icons/arrow.svg" alt="" />
+                    
+                        <div className="hidden items-center gap-[10px] mt-[8px] lg:flex">
+                            <img className="w-[16px] h-[16px]" src="/images/icons/circle_check.svg" alt="" /> 
+                            <p className="text-[14px]/1.5 text-[var(--dark)] font-normal font-[manrope]"> 
+                                This software is designed to work with any site
+                            </p>
+                        </div>
                     </div>
 
                     <div className="flex gap-[14px]">
@@ -43,7 +52,7 @@ export default function Card({title, text, logo, rating, isLock, reviews, price}
                                 <span className="text-[14px] font-semibold">
                                     4.6
                                 </span>
-                                <img className="h-[14px] w-[14px]" src="/images/icons/star.png" alt="" />
+                                <img className="h-[14px] w-[14px]" src="/images/icons/star.svg" alt="" />
                             </div>
                             <span className="font-normal text-[14px] font-[manrope] text-[var(--slate-blue)]">
                                 456 Reviews
@@ -52,10 +61,10 @@ export default function Card({title, text, logo, rating, isLock, reviews, price}
 
                         {
                             isLock 
-                            ?  <button className="w-fit h-[24px] bg-[var(--cyan)] rounded-[15px] flex gap-[8px] items-center px-[12px] 
-                                    text-[14px] text-white font-normal
+                            ?  <button className="w-fit h-[24px] bg-[var(--cyan)] rounded-[15px] flex gap-[7px] items-center px-[10px] 
+                                    text-[14px]/[24px] text-white font-normal
                                 ">
-                                    <img className="w-[11px] h-[14px]" src="/images/icons/mini_lock.png" alt="" />
+                                    <img className="w-[11px] h-[14px]" src="/images/icons/mini_lock.svg" alt="" />
                                     Official software
                                 </button>
                             : null    
@@ -69,9 +78,9 @@ export default function Card({title, text, logo, rating, isLock, reviews, price}
                     {price? `$${price}`: 'Free'}
                 </span>
             </div>
-            <div className="flex items-center gap-[8px]">
-                <img className="w-[16px] h-[16px]" src="/images/icons/circle_check.png" alt="" /> 
-                <p className="text-[14px] text-[var(--dark)] font-normal font-[manrope]"> 
+            <div className="flex items-center gap-[10px] lg:hidden">
+                <img className="w-[16px] h-[16px]" src="/images/icons/circle_check.svg" alt="" /> 
+                <p className="text-[14px]/[24px] text-[var(--dark)] font-normal font-[manrope]"> 
                     This software is designed to work with any site
                 </p>
             </div>
@@ -80,12 +89,12 @@ export default function Card({title, text, logo, rating, isLock, reviews, price}
             ">
                 <div className="flex items-center gap-[14px]">
                     <div className="flex items-center gap-[4px] bg-[var(--light-blue)] w-fit h-[24px] px-[8px] rounded-[12px]">
-                        <span className="text-[14px] font-semibold">
+                        <span className="text-[14px]/[24px] font-semibold">
                             {rating}
                         </span>
-                        <img className="h-[14px] w-[14px]" src="/images/icons/star.png" alt="" />
+                        <img className="h-[14px] w-[14px]" src="/images/icons/star.svg" alt="" />
                     </div>
-                    <span className="font-normal text-[14px] font-[manrope] text-[var(--slate-blue)]">
+                    <span className="font-normal text-[14px]/[24px] font-[manrope] text-[var(--slate-blue)]">
                         {reviews} Reviews
                     </span>
                 </div>
@@ -96,7 +105,7 @@ export default function Card({title, text, logo, rating, isLock, reviews, price}
                 </span>
             </div>
 
-            <p className="text-[var(--slate-blue)] text-[14px] font-normal font-[manrope] hidden
+            <p className="text-[var(--slate-blue)] text-[14px]/[24px] font-normal font-[manrope] hidden
                 lg:block
             ">
                 {text}
